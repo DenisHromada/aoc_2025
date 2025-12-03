7f8072f4f40be41964f62829b12ffb06597229b4
DenisHromada <denhro@seznam.cz>
2025-12-03 08:53:57 +0100

    feat(Day2): Task 1

---
+++ Day2/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("input")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(SplitByComma)
+
+	silly_accum := 0
+
+	for scanner.Scan() {
+		token := scanner.Text()
+
+		token = strings.TrimSpace(token)
+		slices := strings.Split(token, "-")
+
+		start := slices[0]
+		end := slices[1]
+		if len(slices) != 2 {
+			panic("Invalid token format: " + token)
+		}
+
+		start_num, _ := strconv.Atoi(start)
+		end_num, _ := strconv.Atoi(end)
+
+		if start_num > end_num {
+			fmt.Printf("%v - %v\n", start_num, end_num)
+			panic("Invalid range: " + token)
+		}
+
+		for id := start_num; id <= end_num; id++ {
+			if isSilly(id) {
+				silly_accum += id
+			}
+		}
+	}
+
+	fmt.Printf("Silly sum: %v\n", silly_accum)
+}
+
+func isSilly(id int) bool {
+	num_str := strconv.Itoa(id)
+	if len(num_str)%2 != 0 {
+		return false
+	}
+	mid := len(num_str) / 2
+	first_half := num_str[:mid]
+	second_half := num_str[mid:]
+
+	if first_half == second_half {
+		return true
+	}
+	return false
+}
+
+func SplitByComma(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	for i := range data {
+		if data[i] == ',' {
+			return i + 1, data[:i], nil
+		}
+	}
+	if atEOF && len(data) > 0 {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
