02ea121d5d503c0370ce9658e5158e8fb7d71919
DenisHromada <denhro@seznam.cz>
2025-12-03 10:27:04 +0100

    feat(Day3): Task 2

--- Day3/main.go
+++ Day3/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -12,7 +14,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	joltageSum := 0
+	joltagePairSum := 0
+	joltageDozenSum := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -21,10 +24,42 @@ func main() {
 		p_s, p_e := findBatteryPair(line)
 		joltage := (p_s-'0')*10 + p_e - '0'
 
-		joltageSum += int(joltage)
+		joltagePairSum += int(joltage)
+
+		pack_dozen := findBaterryDozen(line)
+		dozen_joltage, err := strconv.Atoi(pack_dozen)
+		if err != nil {
+			panic(err)
+		}
+		joltageDozenSum += dozen_joltage
+
 	}
 
-	println(joltageSum)
+	println("pair sum: ", joltagePairSum)
+	println("dozen sum: ", joltageDozenSum)
+}
+
+func findBaterryDozen(pack string) string {
+	pack_len := len(pack)
+	best_dozen := []byte(pack[pack_len-12:])
+
+	for i := pack_len - 13; i >= 0; i-- {
+
+		candidate := pack[i]
+		fmt.Printf("got pack: %s; and candidate: %s\n", string(best_dozen), string(candidate))
+
+		for j := 0; j < 12; j++ {
+			if candidate >= best_dozen[j] {
+				candidate, best_dozen[j] = best_dozen[j], candidate
+			} else {
+				break
+			}
+		}
+		fmt.Printf("MODIFIED and got pack: %s\n", string(best_dozen))
+
+	}
+
+	return string(best_dozen)
 }
 
 func findBatteryPair(pack string) (byte, byte) {
