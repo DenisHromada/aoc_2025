44429afb5dcb9c9bb96e9b4bd51ecc85cefd5c47
DenisHromada <denhro@seznam.cz>
2025-12-05 06:21:42 +0100

    feat(Day5): Task 1

--- Day5/main.go
+++ Day5/main.go
@@ -4,7 +4,23 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Range struct {
+	Start int
+	End   int
+}
+
+func (r Range) contains(id int) bool {
+	return r.Start <= id && id <= r.End
+}
+
+type All struct {
+	ranges        []Range
+	ingredientIds []int
+}
 
 func main() {
 
@@ -12,23 +28,60 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	_ := parseInput(scanner)
+	parsedInput := parseInput(scanner)
 
-	task1_result := task1(parsed_input)
-	task2_result := task2(parsed_input)
+	task1_result := task1(parsedInput)
+	task2_result := task2()
 
 	fmt.Printf("Task 1 result: %v\n", task1_result)
 	fmt.Printf("Task 2 result: %v\n", task2_result)
 
 }
 
-func parseInput(scanner *bufio.Scanner) {
+func parseInput(scanner *bufio.Scanner) All {
+	input := All{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			break
+		}
+		nums := strings.Split(line, "-")
+		start, e1 := strconv.Atoi(nums[0])
+		end, e2 := strconv.Atoi(nums[1])
 
+		if e1 != nil || e2 != nil {
+			panic("fialed to parse range")
+		}
+
+		new_range := Range{start, end}
+		input.ranges = append(input.ranges, new_range)
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		newId, e := strconv.Atoi(line)
+
+		if e != nil {
+			panic("failed to parse id")
+		}
+
+		input.ingredientIds = append(input.ingredientIds, newId)
+	}
+
+	return input
 }
 
-func task1() int {
-
+func task1(parsedInput All) int {
 	sum := 0
+
+	for i := 0; i < len(parsedInput.ingredientIds); i++ {
+		for j := 0; j < len(parsedInput.ranges); j++ {
+			if parsedInput.ranges[j].contains(parsedInput.ingredientIds[i]) {
+				sum++
+				break
+			}
+		}
+	}
 
 	return sum
 }
