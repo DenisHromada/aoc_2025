0f74902572801356cc3d4e5a9ab266249cf07349
DenisHromada <denhro@seznam.cz>
2025-12-06 06:34:34 +0100

    feat(Day6): Task 1

--- Day6/main.go
+++ Day6/main.go
@@ -4,19 +4,25 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type All struct {
+	lines     [][]int
+	operators []byte
 }
 
 func main() {
 
-	file, _ := os.Open("input_sample")
-	// file, _ := os.Open("input")
+	// file, _ := os.Open("input_sample")
+	file, _ := os.Open("input")
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
 	parsedInput := parseInput(scanner)
+
+	// fmt.Printf(string(parsedInput.operators))
 
 	task1Result := task1(parsedInput)
 	task2Result := task2(parsedInput)
@@ -28,16 +34,68 @@ func main() {
 
 func parseInput(scanner *bufio.Scanner) All {
 	input := All{}
+
 	for scanner.Scan() {
 		line := scanner.Text()
+		slices := strings.Fields(line)
+		lineNumbers := make([]int, 0)
+
+		isOps := false
+
+		for _, el := range slices {
+			num, err := strconv.Atoi(el)
+			if err != nil {
+				input.operators = append(input.operators, el[0])
+				isOps = true
+			} else {
+				lineNumbers = append(lineNumbers, num)
+			}
+		}
+		if !isOps {
+			input.lines = append(input.lines, lineNumbers)
+		} else {
+			break
+		}
 
 	}
+
+	for _, line := range input.lines {
+		for _, el := range line {
+			fmt.Printf("%v ", el)
+		}
+		fmt.Printf("\n")
+	}
+	for _, el := range input.operators {
+		fmt.Printf("%v ", el)
+	}
+	fmt.Printf("\n")
 
 	return input
 }
 
 func task1(parsedInput All) int {
 	sum := 0
+
+	results := make([]int, len(parsedInput.operators))
+	for i, op := range parsedInput.operators {
+		for j, num := range parsedInput.lines {
+			if j == 0 {
+				results[i] = parsedInput.lines[j][i]
+				continue
+			}
+			switch op {
+			case '*':
+				results[i] *= num[i]
+			case '+':
+				results[i] += num[i]
+			}
+		}
+	}
+
+	fmt.Printf("%v\n", results)
+	for _, num := range results {
+		sum += num
+	}
 
 	return sum
 }
