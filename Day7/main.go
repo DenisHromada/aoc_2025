a526dd15bc4877e0e0ef3e1f88b8c6168a78f4a1
DenisHromada <denhro@seznam.cz>
2025-12-07 14:17:27 +0100

    feat(Day7): Task 1

--- Day7/main.go
+++ Day7/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image"
 	"os"
 )
 
 type All struct {
+	source    image.Point
+	splitters map[int]map[int]bool
+	depth     int
 }
 
 func main() {
@@ -27,17 +31,60 @@ func main() {
 }
 
 func parseInput(scanner *bufio.Scanner) All {
-	input := All{}
+	in := All{}
+	in.splitters = make(map[int]map[int]bool)
+
+	lineNumber := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
-	}
 
-	return input
+		for i, c := range line {
+			curPoint := image.Point{i, lineNumber}
+			switch c {
+			case 'S':
+				in.source = curPoint
+			case '^':
+				if in.splitters[lineNumber] == nil {
+					in.splitters[lineNumber] = make(map[int]bool)
+				}
+				in.splitters[lineNumber][i] = true
+			}
+		}
+
+		lineNumber++
+	}
+	in.depth = lineNumber
+
+	return in
 }
 
 func task1(parsedInput All) int {
 	sum := 0
+
+	down := image.Point{0, 1}
+
+	initialBeam := parsedInput.source.Add(down)
+	beams := map[int]bool{}
+	beams[initialBeam.X] = true
+
+	for i := range parsedInput.depth {
+		thisRowSplitters, rowHasSplitters := parsedInput.splitters[i]
+		for j, active := range beams {
+			if !active {
+				continue
+			}
+			if rowHasSplitters {
+				splitter, exists := thisRowSplitters[j]
+				if exists && splitter {
+					beams[j] = false
+					beams[j-1] = true
+					beams[j+1] = true
+					sum++
+				}
+			}
+		}
+	}
 
 	return sum
 }
